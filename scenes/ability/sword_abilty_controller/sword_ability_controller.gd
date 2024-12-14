1a60ee9eae18403e747aef3b02ac657187585503
saturn30 <soso01dev@gmail.com>
2024-12-14 14:09:08 +0900

    검 애니메이션

---
+++ scenes/ability/sword_abilty_controller/sword_ability_controller.gd
@@ -0,0 +1,32 @@
+extends Node
+
+const MAX_RANGE = 150
+
+@export var sword_ability: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Timer.timeout.connect(on_timer_timeout)
+
+func on_timer_timeout():
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+	if player == null:
+		return
+
+	var enemies = get_tree().get_nodes_in_group("enemy")
+	enemies = enemies.filter(func(enemy: Node2D):
+		return enemy.global_position.distance_squared_to(player.global_position) < pow(MAX_RANGE, 2)
+	)
+	if enemies.size() == 0:
+		return
+
+	enemies.sort_custom(func(a:Node2D, b:Node2D):
+		var aDistance = player.global_position.distance_squared_to(a.global_position)
+		var bDistance = player.global_position.distance_squared_to(b.global_position)
+		
+		return aDistance < bDistance
+	)
+	# 현재는 메모리에 있음.
+	var sword_instance = sword_ability.instantiate() as Node2D
+	player.get_parent().add_child(sword_instance)
+	sword_instance.global_position = enemies[0].global_position
